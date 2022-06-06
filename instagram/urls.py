9f521f67ed59a6a6e84d16079e6d8d00b9fea9ec
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 04:02:25 +0300

    initial commit

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-"""gallery URL Configuration
+"""instagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
