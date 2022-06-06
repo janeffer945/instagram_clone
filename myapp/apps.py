a8973bba25e0ee70ae8b7ef4d6441ff0031dfc1e
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 04:22:31 +0300

    add changes

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MigramConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'migram'
+    name = 'myapp'
