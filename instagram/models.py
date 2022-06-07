13779ab4343283e85b3ec7348a76f4d45e782459
janeffer945 <janeffernjeri945@gmail.com>
2022-06-07 17:32:27 +0300

    created listpostview

--- instagram/models.py
+++ instagram/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.utils import timezone
+ 
 # Create your models here.
 class Post(models.Model):
   author= models.ForeignKey('Instagram',on_delete=models.CASCADE)
   image= models. ImageField(blank=True, null=True)
   caption =models.TextField()
   create_date =models.DateTimeField(default=timezone.now)
+   
+  def __str__(self):
+    return self.caption 
 
-class Instagram(models.Model): 
-    name=models.CharField(max_length=225)
-       
+
+class Instagram(models.Model):
+      Comment=models.CharField(max_length=200)
+        
+
+
+  
+
