488ffdbaadd220993c8d06497bc741571b06e952
janeffer945 <janeffernjeri945@gmail.com>
2022-06-08 01:19:41 +0300

    added rendering pyofile page

--- App/views.py
+++ App/views.py
@@ -43,4 +43,118 @@ def create_profile(request):
         if profile:
             messages.success(request,'Profile Created Please Login')
             return redirect("Login")
-    return render(request,'Signup.html')    
+    return render(request,'Signup.html') 
+
+# FOR RENDERING THE PROFILE PAGE
+def profile(request,id=None):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    if id is not None:
+        profile_id = Profile.objects.get(id=id)
+        posts = Post.objects.filter(profile=profile_id)
+        posts_num = posts.count()
+        profile = Profile.objects.get(user=request.user)
+        profileimage = profile.profile_picture.url
+    else:
+        profile_id = Profile.objects.get(user=request.user)
+        posts = Post.objects.filter(user=request.user)
+        posts_num = posts.count()
+        profile = Profile.objects.get(user=request.user)
+        profileimage = profile.profile_picture.url
+    return render(request,'profile.html',{'profile':profile_id,'profileimage':profileimage,'profile_of_user':True,'posts':posts,'posts_num':posts_num})
+
+def Logout(request):
+    logout(request)
+    return redirect("Login")
+
+def search(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    search = request.GET['username']
+    profiles = Profile.objects.filter(user__username__icontains=search)
+    context = {'profiles':profiles,'username':search,"profileimage":profileimage}
+    return render(request,'search.html',context)
+
+# VIEW FOR FOLLOWING THE USER
+def follow(request,id,username):
+    profile = Profile.objects.get(id=id)
+    Login_profile = Profile.objects.get(user=request.user)
+    if request.user in profile.followers.all():
+        profile.followers.remove(request.user)
+        Login_profile.followings.remove(profile.user)
+    else:
+        profile.followers.add(request.user)
+        Login_profile.followings.add(profile.user)
+    return redirect(f'/search?username={username}')
+
+# FOR UPLOADING THE POST
+def upload_post(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    if request.method == 'POST':
+        post = request.FILES['post']
+        profile = Profile.objects.get(user=request.user)
+        posts = Post.objects.create(user=request.user,image=post,profile=profile)
+        if posts:
+            messages.success(request,"POST Uploaded")
+    return render(request,'uploadposts.html',{'profileimage':profileimage})
+
+# FUNCTION FOR LIKING THE POST
+def like_post(request,id):
+    post = Post.objects.filter(id=id)
+    if request.user in post[0].likes.all():
+        post[0].likes.remove(request.user)
+    else:
+        post[0].likes.add(request.user)
+    return redirect("index")
+
+# FOR UPLOADING REEL
+def upload_reel(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    if request.method == 'POST':
+        reel = request.FILES['reel']
+        reels = Reels.objects.create(reel=reel)
+        if reels:
+            messages.success(request,'REEL UPLOADED')
+    return render(request,'uploadreels.html',{'profileimage':profileimage})
+
+# FOR EXPLORING REELS
+def reels(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    reels = Reels.objects.all()
+    return render(request,'reels.html',{'reels':reels,'profileimage':profileimage})
+
+# FOR LIKING THE REEL
+def like_reel(request,id):
+    reel = Reels.objects.get(id=id)
+    if request.user in reel.likes.all():
+        reel.likes.remove(request.user)
+    else:
+          reel.likes.add(request.user)
+    return redirect("reels")
+
+# FOR UPLOADING STORY
+def upload_story(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    profile = Profile.objects.get(user=request.user)
+    profileimage = profile.profile_picture.url
+    if request.method == 'POST':
+        story = request.FILES['story']
+        profile = Profile.objects.get(user=request.user)
+        story_upload = Story.objects.create(story=story,profile=profile)
+        if story_upload:
+            messages.success(request,"STORY UPLOADED")
+    return render(request,'uploadStory.html',{'profileimage':profileimage})       
