7a8c738975319516d75d49e84830b8387dd82e93
janeffer945 <janeffernjeri945@gmail.com>
2022-06-08 16:35:59 +0300

    style css

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'njeri',
+        'PASSWORD':'',
     }
 }
 
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
